1bde705d10dbae87dd864eca854b2498621e843a
S S <368221@AMB00670.local>
2017-11-03 17:36:59 +0530

    added smoke test

--- ci/tasks/smoke-test.sh
+++ ci/tasks/smoke-test.sh
@@ -6,10 +6,9 @@ if [ -z $MOVIEFUN_SERVICE_URL ]; then
   echo "MOVIEFUN_SERVICE_URL not set"
   exit 1
 fi
-apt-get update && apt-get install -y curl
 pushd movie-fun
   echo "Running smoke tests for Attendee Service deployed at $MOVIEFUN_SERVICE_URL"
-  smoke-tests/bin/test $MOVIEFUN_SERVICE_URL
+  MOVIE_FUN_URL=http://movie-fun-pseudoarchaic-vixen.apps.cumuluslabs.io mvn test
 popd
 
 exit 0
